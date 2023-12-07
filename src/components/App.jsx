de73ad294954cd8bd5342f98d94c4d0c61e7c11b
Alyonka556 <142902721+Alyonka556@users.noreply.github.com>
2023-12-07 15:51:18 +0100

    update state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,8 +15,6 @@ export class App extends React.Component {
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
-    name: '',
-    number: '',
   };
 
   handleChange = value => {
